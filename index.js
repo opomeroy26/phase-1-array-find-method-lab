77cb15c27fdad55d23e3ee686cf9cc840e0be3b4
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-02 09:11:09 -0800

    commit tree 81fdf8d04757402226710115e7f4913699f3159b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+
+/*function superBowlWin(record){
+    if ['result'] === "W" {
+        return ['year']
+    } else {
+        return null
+    }
+}
+
+['year', 'result'].find(superBowlWin);
+
+*/
+
+/*function superbowlWin(arr) {
+    for (const record of arr)
+    if (record.result === "W") {
+        return record.year
+    } 
+}
+
+arr.find(superbowlWin) */
+
+
+function superbowlWin(array) {
+    let win = array.find((array) => array.result ==="W");
+    if (win) {
+        return win.year;
+    } else {
+        return undefined
+    }
+}
